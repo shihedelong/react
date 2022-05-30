0d2fdd428b4d1b9eaf52af050654555dfb618059
Hedelong Shi <39339015+shihedelong@users.noreply.github.com>
2022-05-30 13:44:45 +1000

    inverse data flow

--- 12.1-thinking-in-react-review-1/src/components/SearchBar.js
+++ 12.1-thinking-in-react-review-1/src/components/SearchBar.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
 
 export class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this)
+    this.handleInStockChange = this.handleInStockChange.bind(this)
+  }
+
+  handleFilterTextChange(e) {
+    this.props.onFilterTextChange(e.target.value)
+  }
+
+  handleInStockChange(e) {
+    this.props.onInStockChange(e.target.checked)
+  }
   render() {
     const filterText = this.props.filterText
     const inStockOnly = this.props.inStockOnly
     return (
       <form>
-        <input type='text' placeholder='Search...' value={filterText} />
+        <input
+          type='text'
+          placeholder='Search...'
+          value={filterText}
+          onChange={this.handleFilterTextChange}
+        />
         <p>
-          <input type='checkbox' checked={inStockOnly} /> Only show products in
-          stock
+          <input
+            type='checkbox'
+            checked={inStockOnly}
+            onChange={this.handleInStockChange}
+          />{' '}
+          Only show products in stock
         </p>
       </form>
     )
